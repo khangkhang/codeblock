913fa87459a7ea598e0207b00169913c04bccd5b
khangkhang <khangwilliam@gmail.com>
2015-07-26 11:20:00 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+
+    cout << "Hay nhap gia tri canh thu 1: " << endl;
+
+    cin >> x;
+
+    cout << "Hay nhap gia tri canh thu 2: "<< endl;
+    int y;
+
+    cin >> y;
+
+
+
+    cout << "Dien tich tam giac la:" << (x*y)/2 << endl;
+
+
+    return 0;
+}
