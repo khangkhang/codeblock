ae4563d45d00fdedc58ee1df3d24db0fcd078eb4
khangkhang <khangwilliam@gmail.com>
2015-07-26 11:41:44 +0700

    Dien tich hinh thoi

--- main.cpp
+++ main.cpp
@@ -6,20 +6,19 @@ int main()
 {
     int x;
 
-    cout << "Hay nhap gia tri canh thu 1: " << endl;
+    cout << "Hay nhap gia tri duong cheo thu 1: " << endl;
 
     cin >> x;
 
-    cout << "Hay nhap gia tri canh thu 2: "<< endl;
+    cout << "Hay nhap gia tri duong cheo thu 2: "<< endl;
     int y;
 
     cin >> y;
 
 
 
-    cout << "Dien tich chu nhat la:" << x * y << endl;
+    cout << "Dien tich hinh thoi la:" << (x * y) / 2 << endl;
 
-      cout << "Chu vi hinh chu nhat la:" << (x + y ) * 2 << endl;
 
 
     return 0;
